71eecb00895c75eadd494ae09d190848887c0dfd
Everton Mendonça <evertonavilamendonca@gmail.com>
2020-11-10 18:17:30 -0300

    - Primeiro Commit do projeto. - Criando uma aplicação console application padrão com Hello World

---
+++ ConsoleToWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleToWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
