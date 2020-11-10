d77fd713414e47e19a4c6ab75c53115ebb2512e6
Everton Mendonça <evertonavilamendonca@gmail.com>
2020-11-10 19:29:34 -0300

    - Criado um método responsável por escrever o response da aplicação, que lista os filmes para ver. - Adicionada a injeção IApplicationBuilder no método configure, para chamar o Run usando o método FilmesParaVer que foi criado

--- ConsoleToWeb/Startup.cs
+++ ConsoleToWeb/Startup.cs
@@ -1,7 +1,21 @@
-﻿namespace ConsoleToWeb
+﻿using ConsoleToWeb.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleToWeb
 {
     public class Startup
     {
-        public void Configure() { }
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.Run(FilmesParaVer);
+        }
+
+        public Task FilmesParaVer(HttpContext contexto)
+        {
+            var _repo = new FilmeRepositorio();
+            return contexto.Response.WriteAsync(_repo.ParaVer.ToString());
+        }
     }
 }
