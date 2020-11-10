d94c655e78cd68bce71b7b6f1dcb7aac81dc81f6
Everton Mendonça <evertonavilamendonca@gmail.com>
2020-11-10 19:45:03 -0300

    Criado método para fazer roteamento dos paths da aplicação, inicialmente tendo duas rotas, uma para cada lista de filmes. E mais uma rota default que retorna o nome da rota informada

--- ConsoleToWeb/Startup.cs
+++ ConsoleToWeb/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleToWeb.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ConsoleToWeb
@@ -9,7 +10,24 @@ namespace ConsoleToWeb
     {
         public void Configure(IApplicationBuilder app) 
         {
-            app.Run(FilmesParaVer);
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new FilmeRepositorio();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                {"/Filmes/ParaVer",_repo.ParaVer.ToString() },
+                {"/Filmes/Vistos", _repo.Vistos.ToString() }
+            };
+
+            if(caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            return context.Response.WriteAsync(context.Request.Path);
         }
 
         public Task FilmesParaVer(HttpContext contexto)
